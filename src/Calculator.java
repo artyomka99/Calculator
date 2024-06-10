1514993adb85aad3094b2062a732bcc044cdae46
artyomka99 <aatnabaev67@gmail.com>
2024-06-10 23:34:05 +0300

    Калькулятор или люди

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
 
-public class Calculator extends Value {
+public class Calculator {
+    Scanner scan  = new Scanner(System.in);
 
+    public String result () {
+        System.out.println("Enter the number one:");
+        float num1 = scan.nextFloat();
+        System.out.println("Enter the number two:");
+        float num2 = scan.nextFloat();
+        System.out.println("Enter the action:");
+        String action = scan.nextLine();
+        action = scan.nextLine();
+
+        float result = calculator(num1, num2, action);
+        System.out.println("Result: " + result);
+
+        System.out.println("Do you want to do more actions?");
+        if (scan.nextLine().equalsIgnoreCase("yes")) {
+            System.out.println("Enter the number");
+            float num3 = scan.nextFloat();
+            System.out.println("Enter the action:");
+            action = scan.nextLine();
+            action = scan.nextLine();
+            result = calculator(num3, result, action);
+        } else {
+            System.out.println("Goodbye");
+        }
+        return "Result: " + result;
+    }
     private float res;
+
 
     public float addition(float num1, float num2) {
         res = num1 + num2;
@@ -16,15 +44,17 @@ public class Calculator extends Value {
         return res;
     }
     public float division(float num1, float num2) {
-        if (num2 != 0) {
+        if (num2 == 0) {
             System.out.println("You can't divide by zero");;
         }else {
             res = num1 / num2;
         }
         return res;
     }
+
+
     public float calculator (float num1, float num2, String action) {
-        super.setValues(num1, num2, action);
+//        super.setValuesNum(num1, num2, action);
         switch (action){
             case "+":
                 return addition(num1, num2);
@@ -38,8 +68,5 @@ public class Calculator extends Value {
                 System.out.println("Enter the correct action");
         }
         return 0;
-    }
-    public String result(){
-        return "Result: " + res;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,30 @@
 import java.util.Scanner;
 
-public class Main extends Calculator {
+public class Main {
     public static void main(String[] args) {
-        Scanner scan  = new Scanner(System.in);
         Calculator calc = new Calculator();
+        PersonManager pm = new PersonManager();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the number one:");
-        float num1 = scan.nextFloat();
-        System.out.println("Enter the number two:");
-        float num2 = scan.nextFloat();
-        System.out.println("Enter the action:");
-        String action = scan.nextLine();
-        action = scan.nextLine();
-
-        float result = calc.calculator(num1, num2, action);
-        System.out.println(calc.result());
-
-        System.out.println("Do you want to do more actions?");
-        if (scan.nextLine().equalsIgnoreCase("yes")) {
-            System.out.println("Enter the number");
-            float num3 = scan.nextFloat();
-            System.out.println("Enter the action:");
-            action = scan.nextLine();
-            action = scan.nextLine();
-            result = calc.calculator(num3, result, action);
-            System.out.println(calc.result());
-        }else {
-            System.out.println("Goodbye");
+        while (true) {
+            System.out.println("1. Calculator");
+            System.out.println("2. People");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println(calc.result());
+                    break;
+                case 2:
+                    System.out.println(pm.listPeople());
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Enter correct choice");
+            }
         }
     }
 }
