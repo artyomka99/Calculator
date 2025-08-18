37e7d8be4d9fc266e55d8c9826fb5ae05ebba243
artyomka99 <aatnabaev67@gmail.com>
2025-08-18 17:09:18 +0300

    хуй

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,44 @@
-import command.*;
+import command.Command;
 import service.PersonService;
 import service.PetService;
+import org.reflections.Reflections;
 
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
+
 
 public class Main {
 
     static HashMap<Long, Command> commands = new HashMap<>();
     static PersonService personService = new PersonService();
     static PetService petService = new PetService();
+
     static {
-        Command calculator = commands.put(1L, new CalculatorCommand());
-        Command person = commands.put(2L, new PersonCommand());
-        Command pet = commands.put(3L, new GeneratePet());
-        Command saveToJson = commands.put(4L, new SaveToJsonCommand());
-        Command saveToCsv = commands.put(5L, new SaveToCsvCommand());
-        Command loadFromJson = commands.put(6L, new LoadFromJsonCommand());
-        Command loadFromCsv = commands.put(7L, new LoadFromCsvCommand());
-        Command printPerson = commands.put(8L, new PrintPersonCommand());
-        Command printJsonFile = commands.put(9L, new PrintJsonCommand());
-        Command printCsvFile = commands.put(10L, new PrintCsvCommand());
-        Command printPet = commands.put(11L, new PrintePetCommand());
-        Command petToPerson = commands.put(12L, new PetToPersonComannnd());
-        Command findPet = commands.put(13L, new FindPetCommand());
+        try {
+            Reflections reflections = new Reflections("command");
+            Set<Class<? extends Command>> commandClasses = reflections.getSubTypesOf(Command.class);
+
+            for (Class<? extends Command> clazz : commandClasses) {
+                if (!Modifier.isAbstract(clazz.getModifiers())) {
+                    try {
+                        Command command = clazz.getDeclaredConstructor().newInstance();
+                        commands.put(command.getId(), command);
+                    } catch (Exception e) {
+                        System.out.println("Не удалось создать экземпляр команды: " + clazz.getSimpleName()
+                                + ". Проверьте наличие дефолтного конструктора.");
+                    }
+                }
+            }
+            if (commands.isEmpty()) {
+                System.out.println("Команды не найдены. Проверьте пакет command и классы команд.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка при инициализации команд: " + e.getMessage());
+        }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
